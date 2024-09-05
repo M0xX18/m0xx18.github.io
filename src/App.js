a3ccdc605aa271e62413f3a6c33821fd042373dc
M0xX25 <andreseduardo.012@gmail.com>
2024-09-05 15:16:04 -0500

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="text-section">
+            <h1>ANDRÉS EDUARDO GARCÍA</h1>
+            <ChangingText />
+          </div>
+          <div className="image-section">
+            <img src="tu-imagen.jpg" alt="Mi Imagen" className="profile-image" />
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="#home">Inicio</a></li>
+        <li><a href="#about">Acerca de mí</a></li>
+        <li><a href="#projects">Proyectos</a></li>
+        <li><a href="#contact">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+function ChangingText() {
+  const [word, setWord] = useState('seguras');
+
+  useEffect(() => {
+    const words = ['seguras', 'creativas', 'innovadoras']; 
+    const interval = setInterval(() => {
+      setWord((prevWord) => {
+        const currentIndex = words.indexOf(prevWord);
+        return words[(currentIndex + 1) % words.length];
+      });
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <p>Me gusta crear experiencias {word}</p>
+  );
+}
+
 export default App;
+
